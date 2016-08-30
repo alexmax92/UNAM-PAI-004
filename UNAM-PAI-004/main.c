2835f499822734c376deee8360c0c997230d1dc1
luisromero <luisromero@veltsoft.com>
2016-08-30 15:01:39 -0500

    Funcionalidad básica terminada

--- UNAM-PAI-004/main.c
+++ UNAM-PAI-004/main.c
@@ -8,8 +8,35 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+#define NOMBRE "Luis" //Directiva (Definición de Macro)
+#define EDAD 28 //Directiva (Definición de Macro)
+#define MESES (365 / 30.4) //Directiva (Definición de Macro)
+
+long double const PI = 3.14159265359; //Declaración de una constante
+const char Vocales[] = { 'A', 'E', 'I', 'O', 'U' };
+
+
+int main(int argc, const char * argv[])
+{
+    
+    printf("%s tiene %d años.\n\n", NOMBRE, EDAD);
+    printf("Cada año tiene aproximadamente %f meses.\n\n", MESES);
+    
+    //--Tipo de Dato ENUM (Enumerado)
+    enum Colores {ROJO=1, AMARILLO = 2, VERDE=3, AZUL};
+    printf("ROJO = %d\n", ROJO);
+    printf("AMARILLO = %d\n", AMARILLO);
+    printf("VERDE = %d\n", VERDE);
+    printf("AZUL = %d\n\n", AZUL);
+    
+    enum Colores Color;
+    Color = AZUL;
+    
+    printf("El AZUL está representado por el número %d y el compilador lo llenó automáticamente.\n", Color);
+    
+    printf("Pi es una constante equivalente a: %Lf\n", PI);
+    
+    printf("La primera vocal es %c y la última es %c.\n\n", Vocales[0], Vocales[4]);
+    
     return 0;
 }
