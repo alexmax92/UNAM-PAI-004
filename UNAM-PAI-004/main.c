d97c5c7ee503bf3e7e450f27a1262fe1801785d6
luisromero <luisromero@veltsoft.com>
2016-08-30 14:00:33 -0500

    Initial Commit

---
+++ UNAM-PAI-004/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  UNAM-PAI-004
+//
+//  Created by Luis Romero on 8/30/16.
+//  Copyright © 2016 Luis Romero. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
